5b6ea6f782a7d62e4a1c4528823555cfcebf3c4e
root <root@DESKTOP-2H07VP6.localdomain>
2026-05-22 08:20:28 +0200

    resource usage in web.php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/check-status-render-xyz', function() {
+    $laravelMemory = round(memory_get_usage(true) / 1024 / 1024, 2);
+    $totalMem = 'Onbekend (Geen Linux)';
+    
+    if (file_exists('/proc/meminfo')) {
+        $meminfo = file_get_contents('/proc/meminfo');
+        $totalMem = substr($meminfo, 0, 200); 
+    }
+
+    return response()->json([
+        'laravel_ram_mb' => $laravelMemory,
+        'container_meminfo_raw' => explode("\n", $totalMem)
+    ]);
+});
